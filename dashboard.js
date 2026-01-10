2e4cf9974ba1b458723287407e26550eadbfbde3
Zyonel <elxionel@gmail.com>
2026-01-10 21:33:18 +0100

    lkl

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,7 @@
 import { auth, db } from "./firebase.js";
+
+import { setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 //import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import {
@@ -237,9 +240,9 @@ function startVisit(task) {
 }
 
 //coin system
-let coins = 0;
+/*let coins = 0;
 
 function addCoins(amount) {
   coins += amount;
   document.getElementById("balanceValue").textContent = coins;
-}
+}*/
