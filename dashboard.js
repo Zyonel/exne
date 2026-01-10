d11e9abe287dd79891ff43c285043a0782af24e9
Zyonel <elxionel@gmail.com>
2026-01-10 04:04:07 +0100

    asd

--- dashboard.js
+++ dashboard.js
@@ -17,10 +17,13 @@ onAuthStateChanged(auth, async (user) => {
 
   const data = snap.data();
   avatar.src = data.photoURL;
-  welcomeText.textContent = `Welcome, ${data.username}`;
+  welcomeText.textContent = `document.getElementById("welcomeText"), ${data.username}`;
+
+//  welcomeText.textContent = `document.getElementById("welcomeText"), ${data.username}`;
+
 });
 
-// Bottom nav logic
+// Navigation
 document.querySelectorAll(".nav-btn").forEach(btn => {
   btn.addEventListener("click", () => {
     document.querySelectorAll(".nav-btn").forEach(b => b.classList.remove("active"));
@@ -30,10 +33,24 @@ document.querySelectorAll(".nav-btn").forEach(btn => {
 
     if (page === "home") {
       content.innerHTML = "<h2>Home</h2>";
-    } else if (page === "transactions") {
+    } 
+    else if (page === "transactions") {
       content.innerHTML = "<h2>Transactions</h2>";
-    } else if (page === "profile") {
+    } 
+    else if (page === "profile") {
       content.innerHTML = "<h2>Profile</h2>";
     }
+  });
+});
+
+/* sub main */
+document.querySelectorAll(".dash-card").forEach(card => {
+  card.addEventListener("click", () => {
+    const action = card.dataset.action;
+
+    if (action === "rewards") alert("Daily Rewards");
+    if (action === "tasks") alert("Daily Tasks");
+    if (action === "games") alert("Mini Games");
+    if (action === "watch") alert("Watch & Earn");
   });
 });
